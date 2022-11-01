0e9e0b644280b3bce5c67e8d02e4b9c823970c02
brandonkimmmm <bkim2490@gmail.com>
2022-11-01 17:34:51 +0900

    post servie update

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -46,12 +46,7 @@ export class PostService {
                                 comments: true
                             }
                         },
-                        topic: {
-                            select: {
-                                id: true,
-                                display_title: true
-                            }
-                        }
+                        topic: true
                     }
                 });
                 const postVotes = await prisma.vote.groupBy({
